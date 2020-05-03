c54ae5a39d84a5b65eebfcd63dc50e8db28100b7
yayayahei <mmwcbz@msn.cn>
2020-05-03 22:44:24 +0800

    add create indicator button

---
+++ app/src/main/java/com/yayayahei/ihealthapp/CreateIndicatorActivity.kt
@@ -0,0 +1,13 @@
+package com.yayayahei.ihealthapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateIndicatorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_indicator)
+        setTitle(R.string.create_indicator)
+    }
+}
--- app/src/main/java/com/yayayahei/ihealthapp/MainActivity.kt
+++ app/src/main/java/com/yayayahei/ihealthapp/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
 
 const val EXTRA_MESSAGE = "com.yayayahei.ihealthapp.MESSAGE"
 
@@ -14,12 +13,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun sendMessage(view: View) {
-        val editText = findViewById<EditText>(R.id.editText)
-        val message = editText.text.toString()
-        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
-            putExtra(EXTRA_MESSAGE, message)
-        }
+    fun gotoAddIndicatorActivity(view: View) {
+        val intent = Intent(this, CreateIndicatorActivity::class.java)
         startActivity(intent)
     }
 }
