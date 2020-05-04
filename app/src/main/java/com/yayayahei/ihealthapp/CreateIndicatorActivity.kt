59492f65ab3e407913e3ab95a16e12a264b8ce2c
yayayahei <mmwcbz@msn.cn>
2020-05-04 22:52:07 +0800

    add ToolBar with Done button

--- app/src/main/java/com/yayayahei/ihealthapp/CreateIndicatorActivity.kt
+++ app/src/main/java/com/yayayahei/ihealthapp/CreateIndicatorActivity.kt
@@ -2,12 +2,33 @@ package com.yayayahei.ihealthapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 
 class CreateIndicatorActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_indicator)
-        setTitle(R.string.create_indicator)
+        val toolbar = findViewById<Toolbar>(R.id.edit_toolbar)
+//        toolbar.setTitle(R.string.create_indicator)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.edit, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?) = when (item?.itemId) {
+        R.id.action_done -> {
+//            todo: save indicator
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
     }
 }
