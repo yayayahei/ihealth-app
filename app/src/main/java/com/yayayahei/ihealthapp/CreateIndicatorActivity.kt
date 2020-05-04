dfc9bd8736eee64a6e8ba84ae6485f810da6ce65
yayayahei <mmwcbz@msn.cn>
2020-05-05 00:52:16 +0800

    add custom action bar for create indicator activity

--- app/src/main/java/com/yayayahei/ihealthapp/CreateIndicatorActivity.kt
+++ app/src/main/java/com/yayayahei/ihealthapp/CreateIndicatorActivity.kt
@@ -1,34 +1,41 @@
 package com.yayayahei.ihealthapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
+
 class CreateIndicatorActivity : AppCompatActivity() {
+
+    lateinit var discard_button: TextView
+    lateinit var save_button: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_indicator)
+
+
+
+
+        InitializeFields()
+    }
+
+    fun InitializeFields() {
         val toolbar = findViewById<Toolbar>(R.id.edit_toolbar)
-//        toolbar.setTitle(R.string.create_indicator)
         setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.edit, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
+        supportActionBar?.setDisplayShowCustomEnabled(true)
 
-    override fun onOptionsItemSelected(item: MenuItem?) = when (item?.itemId) {
-        R.id.action_done -> {
-//            todo: save indicator
-            true
-        }
-        else -> {
-            super.onOptionsItemSelected(item)
-        }
+        val mInflater = LayoutInflater.from(this)
+        val customAction: View = mInflater.inflate(R.layout.edit_custom_action_bar, null)
+        supportActionBar?.customView = customAction
+
+        discard_button = customAction.findViewById(R.id.discard_button)
+        discard_button.setOnClickListener(View.OnClickListener { finish() })
+        save_button = customAction.findViewById(R.id.save_button)
+        save_button.setOnClickListener(View.OnClickListener { finish() })
     }
 }
