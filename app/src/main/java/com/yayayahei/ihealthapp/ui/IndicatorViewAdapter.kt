db5b0802659183cb37c42b6f8860309ca82c2b74
yayayahei <mmwcbz@msn.cn>
2020-05-16 22:37:54 +0800

    add arrow at indicator list

--- app/src/main/java/com/yayayahei/ihealthapp/ui/IndicatorViewAdapter.kt
+++ app/src/main/java/com/yayayahei/ihealthapp/ui/IndicatorViewAdapter.kt
@@ -3,24 +3,28 @@ package com.yayayahei.ihealthapp.ui
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.yayayahei.ihealthapp.R
 import com.yayayahei.ihealthapp.persistence.Indicator
 
 class IndicatorViewAdapter(private val indicators: List<Indicator>) :
-    RecyclerView.Adapter<IndicatorViewAdapter.MyViewHolder>() {
-    class MyViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+    RecyclerView.Adapter<IndicatorViewAdapter.IndicatorViewHolder>() {
+    //    class MyViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+    class IndicatorViewHolder(val indicatorView: ConstraintLayout) :
+        RecyclerView.ViewHolder(indicatorView)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val textView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.indicator_item_in_list, parent, false) as TextView
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IndicatorViewHolder {
+        val indicatorView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.indicator_item_in_list, parent, false) as ConstraintLayout
 
-        return MyViewHolder(textView)
+        return IndicatorViewHolder(indicatorView)
     }
 
     override fun getItemCount() = indicators.count()
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.textView.text = indicators[position].name
+    override fun onBindViewHolder(holder: IndicatorViewHolder, position: Int) {
+        val textView = holder.indicatorView.findViewById<TextView>(R.id.indicatorItemName)
+        textView.text = indicators[position].name
     }
 
 }
