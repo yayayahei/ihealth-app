9990057fe12acd51481ddcb07cdc963726ba1e9d
yayayahei <mmwcbz@msn.cn>
2020-05-31 22:51:38 +0800

    show toolbar at EditGaugeActivity

--- app/src/main/java/com/yayayahei/ihealthapp/ui/EditGaugeActivity.kt
+++ app/src/main/java/com/yayayahei/ihealthapp/ui/EditGaugeActivity.kt
@@ -2,11 +2,21 @@ package com.yayayahei.ihealthapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import com.yayayahei.ihealthapp.R
 
 class EditGaugeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_gauge)
+
+        showAppBar()
+
+    }
+    private fun showAppBar() {
+        val toolbar = findViewById<Toolbar>(R.id.edit_gauge_toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+//        supportActionBar?.title = indicatorName
     }
 }
